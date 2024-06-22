8b2484d555d6f2ac49d9ea0ccffaf409ca883112
luishecastilho <manocastilho07@gmail.com>
2024-06-22 12:29:58 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\{
-    AuthController,
-    IndexController
-};
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\IndexController;
 
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
-Route::post('signup', [AuthController::class, 'signup'])->name('signup');
-Route::post('login', [AuthController::class, 'login'])->name('login');
-
-// authed
 Route::get('quotes', [IndexController::class, 'quotes'])->name('quotes');
